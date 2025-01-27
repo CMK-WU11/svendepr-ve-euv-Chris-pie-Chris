87d7e42a713b576bf7e1e87d1ccf12a9a93bf72d
wu11-christian-tuyen <christiant1996@hotmail.com>
2025-01-27 15:38:14 +0100

    Dag 1, sidste push (med små styling)

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
       <section className="relative bg-cover bg-center w-screen h-screen bg-[url('/splash-image.jpg')]">
           <div className="flex justify-center items-center bottom-0">
-            <Link href="/Aktiviteter"><button className="">Kom i gang</button></Link>
+            <Link href="/Aktiviteter"><button className="bg-[#5E2E53] w-48 h-10 text-white rounded-md">Kom i gang</button></Link>
           </div>
       </section>
   );
